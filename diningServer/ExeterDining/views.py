51000b0c1d04d640625386755aef0159e03f1b33
bbae07 <cbae@exeter.edu>
2016-03-19 15:13:41 +0900

    crawler

--- diningServer/ExeterDining/views.py
+++ diningServer/ExeterDining/views.py
@@ -10,13 +10,22 @@ from push_notifications.models import APNSDevice
 def restaurant_list(request, format=None):
 
     if request.method == 'GET':
-        crawl = Crawler()
-        wBreakfast = crawl.getTodayMeals()[0].breakfast
-        wLunch = crawl.getTodayMeals()[0].lunch
-        wDinner = crawl.getTodayMeals()[0].dinner
-        eBreakfast = crawl.getTodayMeals()[1].breakfast
-        eLunch = crawl.getTodayMeals()[1].lunch
-        eDinner = crawl.getTodayMeals()[1].dinner
+        # crawl = Crawler()
+        # wetherell = crawl.getTodayMeals()[0]
+        # elmStreet = crawl.getTodayMeals()[1]
+        # wBreakfast = wetherell.breakfast;
+        # wLunch = wetherell.lunch;
+        # wDinner = wetherell.dinner;
+        # eBreakfast = elmStreet.breakfast;
+        # eLunch = elmStreet.lunch;
+        # eDinner = elmStreet.dinner;
+
+        wBreakfast = ['Chilled Fruit Juices']
+        wLunch = ['Home Fried Potatoes','Yo-nola Bar','Soup du Jour']
+        wDinner = ['Steamed Vegetable','Manager Choice Dessert']
+        eBreakfast = ['Pete & Gerrys Organic Boiled Eggs','Ham and Cheese Omelet','Corned Beef Hash']
+        eLunch = ['Roast Pork Loin','Pepperoni Calzone from the Hearth','Chicken Caesar Salad']
+        eDinner = ['Edamame Stir Fry','Sweet Potato Fries','Roasted Vegetable','Homemade Waffles','Steel Cut Oatmeal']
         return JsonResponse(
         {'Restaurants':
              [{'Wetherell':
@@ -29,7 +38,7 @@ def sendPushMessage(request,):
     if request.method == 'GET':
         push = Push()
         crawl = Crawler()
-        push.sendMessage(crawl.todayStatus)
+        push.sendMessage(1)
         return JsonResponse({'PUSH':'SUCCESS'})
 
 def getAPNSToken(request):
